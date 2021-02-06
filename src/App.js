9620367cdc92a1b36325ae40e89fd0ed11883617
LeeJeongWoon <ljwoon94@gmail.com>
2021-02-06 21:14:41 +0900

    React update 공부

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Control from "./components/Control";
 import Subject from "./components/Subject";
 import ReadContent from "./components/ReadContent";
 import CreateContent from "./components/CreateContent";
-
+import UpdateContent from "./components/UpdateContent";
 // 컴포넌트 생성 템플릿이라 생각하면 된다.
 
 class App extends Component {
@@ -28,26 +28,26 @@ class App extends Component {
       ]
     }
   }
-  
-  render(){
-    console.log('App render')
+  getReadContent(){
+    var i = 0;
+      while(i < this.state.contents.length){
+        var data = this.state.contents[i];
+        if(data.id === this.state.selected_content_id) {
+          return data;
+          break;
+        }
+        i = i + 1;
+      }
+  }
+  getContent(){
     let _title, _desc, _article = null;
     if(this.state.mode ==='welcome'){
       _title = this.state.welcome.title;
       _desc = this.state.welcome.desc;
       _article = <ReadContent title={_title} desc={_desc}></ReadContent>
     }else if(this.state.mode ==='read'){
-      let i=0;
-      while(i<this.state.contents.length){
-        let data = this.state.contents[i];
-        if(data.id === this.state.selected_content_id){
-          _title = data.title;
-          _desc = data.desc;
-          break;
-        }
-        i = i+1;
-      }
-      _article =<ReadContent title={_title} desc={_desc}></ReadContent>
+      var _content= this.getReadContent();
+      _article =<ReadContent title={_content._title} desc={_content._desc}></ReadContent>
     } else if (this.state.mode === 'create'){
       _article = <CreateContent onSubmit={function(_title, _desc){
         // add content to this.state.contents
@@ -71,7 +71,32 @@ class App extends Component {
         });
         console.log(_title, _desc);
       }.bind(this)}></CreateContent>
+    } else if (this.state.mode === 'update'){
+      _content = this.getReadContent();
+      _article = <UpdateContent data={_content} onSubmit=
+        {function(_id,_title, _desc){
+          var _contents = Array.from(this.state.contents);
+          let i = 0;
+          while(i<_contents.length){
+            if(_contents[i].id === _id){
+              _contents[i] = {id:_id, title:_title, desc:_desc};
+              break;
+            }
+            i = 1+1;
+          }
+          
+          this.setState({
+            contents:_contents,
+            mode:'read'
+        });
+        console.log(_title, _desc);
+      }.bind(this)}></UpdateContent>
     }
+    return _article;
+  }
+  render(){
+    console.log('App render');
+    
     return (
       <div className="App">
         <Subject 
@@ -119,7 +144,7 @@ class App extends Component {
             mode:_mode
           });
         }.bind(this)}></Control>
-        {_article}
+        {this.getContent()}
       </div>
     );
   }
