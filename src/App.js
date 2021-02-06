1917d55aa1de1f80925bc369fac08e795880f356
LeeJeongWoon <ljwoon94@gmail.com>
2021-02-06 22:06:28 +0900

    react delete 공부, 리팩토링

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import CreateContent from "./components/CreateContent";
 import UpdateContent from "./components/UpdateContent";
 // 컴포넌트 생성 템플릿이라 생각하면 된다.
 
+
 class App extends Component {
   constructor(props){
     //constructor는 props를 사용하기 위해 필요
@@ -15,16 +16,16 @@ class App extends Component {
     //컴포넌트가 실행될때 render()보다 먼저 실행되 초기화를 담당
     this.max_content_id = 3;
     //데이터를 추가할때 사용하는 변수 state에 넣을 필요가ㅏ 없다.
-    this.state ={
-      mode:'read',
+    this.state = {
+      mode:'welcome',
       // 페이지가 어디 경로인지 확인용으로 mode를 만듦
       selected_content_id:2,
-      subject:{title:'WEB',sub:'World Wide Web!'},
-      welcome:{title:'Welcome', desc:'Hello,React!!'},
+      subject:{title:'WEB', sub:'World Wide Web!'},
+      welcome:{title:'Welcome', desc:'Hello, React!!'},
       contents:[
-        {id:1, title:'HTML',desc:'HTML is for information ...'},
-        {id:2, title:'CSS',desc:'CSS is for design ...'},
-        {id:3, title:'JavaScript',desc:'JavaScript is for interactive ...'}
+        {id:1, title:'HTML', desc:'HTML is for information'},
+        {id:2, title:'CSS', desc:'CSS is for design'},
+        {id:3, title:'JavaScript', desc:'JavaScript is for interactive'}
       ]
     }
   }
@@ -40,15 +41,15 @@ class App extends Component {
       }
   }
   getContent(){
-    let _title, _desc, _article = null;
-    if(this.state.mode ==='welcome'){
+    var _title, _desc, _article = null;
+    if(this.state.mode ===  'welcome'){
       _title = this.state.welcome.title;
       _desc = this.state.welcome.desc;
       _article = <ReadContent title={_title} desc={_desc}></ReadContent>
-    }else if(this.state.mode ==='read'){
-      var _content= this.getReadContent();
-      _article =<ReadContent title={_content._title} desc={_content._desc}></ReadContent>
-    } else if (this.state.mode === 'create'){
+    } else if(this.state.mode === 'read'){
+      var _content = this.getReadContent();
+      _article = <ReadContent title={_content.title} desc={_content.desc}></ReadContent>
+    } else  if(this.state.mode === 'create'){
       _article = <CreateContent onSubmit={function(_title, _desc){
         // add content to this.state.contents
         this.max_content_id = this.max_content_id+1;
@@ -60,53 +61,47 @@ class App extends Component {
         //   );
         //push랑 concat의 차이
         //push는 배열 원본을 바꾸는거면 concat은 배열원본을 그대로 두고 추가하는것이다.
-        var newContents = Array.from(this.state.contents);
-        //Array.from()은 배열을 다른 변수에 넣을때 사용.
-        //내용은 같지만 다른 배열 생성
-        //Object.assing({},a)내용은 같지만 다른 객체를 만들때 사용.
-        newContents.push({id:this.max_content_id, title:_title, desc:_desc});
-        
+        var _contents = Array.from(this.state.contents);
+        _contents.push({id:this.max_content_id, title:_title, desc:_desc});
         this.setState({
-          contents:newContents
+          contents:_contents,
+          mode:'read',
+          selected_content_id:this.max_content_id
         });
-        console.log(_title, _desc);
       }.bind(this)}></CreateContent>
-    } else if (this.state.mode === 'update'){
+    } else  if(this.state.mode === 'update'){
       _content = this.getReadContent();
-      _article = <UpdateContent data={_content} onSubmit=
-        {function(_id,_title, _desc){
+      _article = <UpdateContent data={_content} onSubmit={
+        function(_id, _title, _desc){
           var _contents = Array.from(this.state.contents);
-          let i = 0;
-          while(i<_contents.length){
-            if(_contents[i].id === _id){
+          var i = 0;
+          while(i < _contents.length){
+            if(_contents[i].id === _id) {
               _contents[i] = {id:_id, title:_title, desc:_desc};
               break;
             }
-            i = 1+1;
+            i = i + 1;
           }
-          
           this.setState({
             contents:_contents,
             mode:'read'
-        });
-        console.log(_title, _desc);
-      }.bind(this)}></UpdateContent>
+          });
+        }.bind(this)}></UpdateContent>
     }
     return _article;
   }
-  render(){
+  render() {
     console.log('App render');
-    
     return (
       <div className="App">
         <Subject 
           title={this.state.subject.title} 
           sub={this.state.subject.sub}
-          onChangePage= {function(){
+          onChangePage={function(){
             this.setState({mode:'welcome'});
           }.bind(this)}
           // onChangePage 컴포넌트안에 클릭을 했을시 함수를 호출하는 이벤트
-        >  
+        >
         </Subject>
         {/* <header>
           <h1><a href ="/" onClick={function(e){
@@ -137,16 +132,37 @@ class App extends Component {
               selected_content_id:Number(id)
             });
           }.bind(this)} 
-          data= {this.state.contents}
+          data={this.state.contents}
         ></TOC>
         <Control onChangeMode={function(_mode){
-          this.setState({
-            mode:_mode
-          });
+          if(_mode === 'delete'){
+            if(window.confirm('really?')){
+              var _contents = Array.from(this.state.contents);
+              var i = 0;
+              while(i < _contents.length){
+                if(_contents[i].id === this.state.selected_content_id){
+                  _contents.splice(i,1);
+                  break;
+                }
+                i = i + 1;
+              }
+              this.setState({
+                mode:'welcome',
+                contents:_contents
+              });
+              alert('deleted!');
+            }
+          } else {
+            this.setState({
+              mode:_mode
+            });
+          }
+          
         }.bind(this)}></Control>
         {this.getContent()}
       </div>
     );
   }
 }
+
 export default App;
